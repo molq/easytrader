b47e96d0ffbf71b439ae4c24d0b31c4bad3be2e8
molq <369123798@qq.com>
2025-12-03 22:57:23 +0800

    雪球跟单模拟可以 启动了

--- tests/mytest.py
+++ tests/mytest.py
@@ -1,12 +1,36 @@
 import easytrader
+from easytrader.log import logger
 
 # 1. 创建交易客户端
-user = easytrader.use("xq")  # 雪球
+# user = easytrader.use("xq")  # 雪球
 # user = easytrader.use('ht_client')  # 华泰
-
+user = easytrader.use('miniqmt')  # miniqmt
 # 2. 登录/连接
 # user.prepare("../config.json")
 # 或 user.connect('client_path')
+
+# user.connect(
+#     miniqmt_path=r"D:\soft\国金证券QMT交易端\userdata_mini",  # QMT 客户端下的 miniqmt 安装路径
+   
+#     stock_account="8887670918",  # 资金账号
+    
+#     trader_callback=None   # 默认使用 `easytrader.miniqmt.DefaultXtQuantTraderCallback`
+# )
+
+
+user.connect(
+    
+    miniqmt_path=r"D:\国金QMT交易端模拟\userdata_mini",
+    stock_account="62500888",  # 资金账号
+    trader_callback=None   # 默认使用 `easytrader.miniqmt.DefaultXtQuantTraderCallback`
+)
+# 获取资金状况
+
+logger.error("获取资金状况: %s", user.balance)
+
+# 获取持仓
+
+logger.error("获取资金获取持仓状况: %s", user.position)
 
 # 3. 执行交易
 # user.buy("000001", price=10.0, amount=100)
@@ -14,6 +38,6 @@ user = easytrader.use("xq")  # 雪球
 # 4. 创建跟单策略
 follower = easytrader.follower(platform="xq")
 follower.login(
-    cookies="cookiesu=661760411130159; device_id=8fc5b1eaa2f27255f3a57e733348dad7; smidV2=20251014110531131e693cfdee3bae60bbd164c8525108003d1aa9354a0c590; s=ab1o4yamzq; Hm_lvt_1db88642e346389874251b5a1eded6e3=1760411131,1762247791,1762508680; HMACCOUNT=9E10F8B43ADD912D; remember=1; xq_a_token=956206e2f2bc462784e9ea0c5ba034993af9a781; xqat=956206e2f2bc462784e9ea0c5ba034993af9a781; xq_id_token=eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiJ9.eyJ1aWQiOjU5NzQ3MTIxMTUsImlzcyI6InVjIiwiZXhwIjoxNzY1MTAwNzA3LCJjdG0iOjE3NjI1MDg3MDczMDgsImNpZCI6ImQ5ZDBuNEFadXAifQ.TvS2EN3oN1EosuBE29N2-s4yCTln5lcyLjC_2Bm_N7pH_hEr7lmMLjEEKNDsY6XWOlTTeSybA61sY2hA7tQf2TJGE3lu0a9RHIun-rqegoLEEoQx020aGYXka4D4-Iv8_JwjLSaoMesMx0w4DAD0r3pxsFlOLshZx0KqK50gJuzJZs0RexxuOHMgLx_-uxvuNK2WYEvXMGTUebkArVi9CkI5nNTg0qRgCZYCksb-a6z-w38zV-JohL5SC204895PQ-NUd6bzE3pGS2ZfxYx27dkf9jChPsft3EH16dtU9yKy90ze_DcdleqPPeE3UAlQDizhPJSCSHMESWpj5Aopkg; xq_r_token=666d0ff6146d8a919afd7385ef12a3b9ead1121c; xq_is_login=1; u=5974712115; bid=71e7421c8bdd04512ff569539a2640a6_mhoo6kkt; __utmc=1; __utmz=1.1762508716.1.1.utmcsr=(direct)|utmccn=(direct)|utmcmd=(none); acw_tc=3ccdc15217629478976328112e596972f53e0fc7f991d78d01a9a6be3a9857; is_overseas=0; .thumbcache_f24b8bbe5a5934237bbc0eda20c1b6e7=E5osJyLm2+miOPNGrSzfo2GciYhidX5iAwG0qiQ71CuCQVBx2kYKUfBN1/cj+8Sc0Ay3MiBAVu7SrMldNKsJJQ%3D%3D; __utma=1.1586223490.1762508716.1762508716.1762949643.2; __utmt=1; __utmb=1.1.10.1762949643; Hm_lpvt_1db88642e346389874251b5a1eded6e3=1762949684; ssxmod_itna=1-Qqf20KDKYvqAxYqYQiQLPAjmeTpDuDUx0dGM0Deq7tDRDFqAPYDHKIEC75E4NGFCnDDtqlr80tDlrh0YDSxD=7DK4GTwGgi7_154pGDT3dhe8xprqFmYEi5tbfI6kBjOLKbMy2HLXzbUiqGImDG2DYoDCqDS0DD9KrArDYAfDBYD74G_DDeDiObDGdW52DDFBjRQMWiQFW45geDE0YrF0DDgRPD1b1nbgWi3iPGfbWpi2AiDbMwp2GPtx0fCaOODzdrDboodX/8Dtq7PdCINPpWQnt6MB_dYmmGpKG4hD3h_C7mYBh433MxwB4eD4cLQM7ex3hwiidn1Mw457ezdCwdrpdbMYeorz9xiwYQoBd_m43m5/DYqOD4rYbmDHBxhD4eehViDD; ssxmod_itna2=1-Qqf20KDKYvqAxYqYQiQLPAjmeTpDuDUx0dGM0Deq7tDRDFqAPYDHKIEC75E4NGFCnDDtqlr80YD==32YIts9jop1Z/PlDrh4Neck4xD"
+    cookies="cookiesu=911741358545830; smidV2=20250307224226891d4776dee78eb8916ec6d39dccc26100de281ff8d605f20; device_id=5ba82b71970b271d37e98b61d26e8eae; s=bp134pqcwo; Hm_lvt_1db88642e346389874251b5a1eded6e3=1741358546,1742048088; u=5974712115; bid=71e7421c8bdd04512ff569539a2640a6_min9hua8; acw_tc=1a0c652117647725922116198ea5bc7f3f09b8cd7b1bd92880271442095025; remember=1; xq_a_token=64aca9edbac17928f087765e51a10f5ac84a5656; xqat=64aca9edbac17928f087765e51a10f5ac84a5656; xq_id_token=eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiJ9.eyJ1aWQiOjU5NzQ3MTIxMTUsImlzcyI6InVjIiwiZXhwIjoxNzY3Mjc0MjEwLCJjdG0iOjE3NjQ3NzI2MTk4MzEsImNpZCI6ImQ5ZDBuNEFadXAifQ.NJdqZhoYBkbgCSyEdpb7ZoF3N6VlPE6AKPCd7fDR7SSv_M_T53MNyeojTIYbxba48iU4RqVxajdpgmFgCFQgCqJDLKadtbgDdhB1ArK8IE-Kd9_VDOkVI2gaUc1XwSvXtSqvMep-hT-otzd0bwMTPbT3VI7E1-T5KA0IeN_R0r3hQW8-J9Qx8SMgAKleDNZw9aqD3aFBGwkDAdvjxFIxAXpkScSbkMGXxU2qdR1hdVKrW1zGfYC-rEi2yZy0EkneAf7pWUCZnXdYItJEQIPuXEYYLMtsMQSGxt676CX9ZtnV1pfU9cPO2s8JFTL9tQK9upK9XSFEDlKAAHAYqgU3vg; xq_r_token=a30379137e3e11d0eaf8fb6eda6c8931f854e968; xq_is_login=1; .thumbcache_f24b8bbe5a5934237bbc0eda20c1b6e7=FeAVeOoEXFkP/uhwBR37NcePjLKbDplQHuL4JjWiwIppyFaS8rIlmPpZYJp2z5NuciC6zjYkncNTK0OgpZiK5Q%3D%3D; _c_WBKFRo=2vlqk3EfftMIGBiu188PcEpzGgEbisnsnZNqm9yB; _nb_ioWEgULi=; acw_sc__v3=69304b1a9d45ff36243f9cbc0867e7662457b3e5; ssxmod_itna=1-iqRxR7qQqeqEG0CiDOii=DkAeiOeDXDULqiQGgADFqAPqDHWzOUADODUokD5HoTxDC4R0DYPyD05=iDnqD8jDQeDvD28iiD0Cdqt6HA27ExCe7DRGK6d5QG60xdOAaU8qoXeQTbcO7uGkDfGFBQ5xemekCOeDHxi8DBI4KroYDeeaDCeDQxirDD4DADibr4D11DDkD04AIIff_4GWDm4ADWPDYxDrGfoDRxxD0ZnwDQ9oG9wFwfPDBEpn81otk1dXN4C55sf4lRDqG4G1vD0HH7zuqszS8g3Xs5z6oEoDXONDvE85XKg2X7W08ac3=9ODGGrxxeH7o=YYeB=VDhMi=hEiYODQGYq0D8hxMBKoCmCxDirRrf2QD_MEN/gND4_P3m/emljmp0iSlp4r5rIw1nwsAD4ApK0iePnMQbqiDxD; ssxmod_itna2=1-iqRxR7qQqeqEG0CiDOii=DkAeiOeDXDULqiQGgADFqAPqDHWzOUADODUokD5HoTxDC4R0DYPGDDPYD0DfgTAt1RBItz/DbGOqEzO3D"
 )
-follower.follow(users=user, strategies=["ZH3542839"], initial_assets=1000000)
+follower.follow(users=user, strategies=["ZH3381319"], initial_assets=100000)
